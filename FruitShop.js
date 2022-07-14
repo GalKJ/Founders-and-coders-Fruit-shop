de47b6ea401b4156ef68ec1b6056dcb798336c82
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-07-14 16:39:38 +0100

    add item cards
    add item cards as html elements rather than rendering from javascript

--- FruitShop.js
+++ FruitShop.js
@@ -1,2 +1,11 @@
-//Select Elements
-const productEl = document.querySelector(".products");
+// User basket
+
+let userBasket = {
+    banana: 0,
+    berries: 0,
+    grapes: 0,
+    watermelon: 0,
+};
+
+
+
