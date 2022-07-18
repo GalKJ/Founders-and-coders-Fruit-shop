34938c362ed64c38103c26e0620befda64c5d0f0
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-07-18 13:13:47 +0100

    add dataset to fruit card buttons
    dataset added to buttons to allow access to individual fuit card buttos

--- FruitShop.js
+++ FruitShop.js
@@ -5,7 +5,28 @@ let userBasket = {
     berries: 0,
     grapes: 0,
     watermelon: 0,
-};
+    };
+    
+// Select elements 
+const basketButton = document.querySelectorAll(".basket-button button");
+
+// Function to run when event handler fires when basket button in clicked
+
+function logTarget(e) {
+    console.log(e.target.dataset.fruit);
+}
+
+// Function to add event listener to add to basket buttons on fruit cards 
+function addEventListenerBasket(button) {
+    button.addEventListener('click', logTarget)
+}
+
+// Add event listener on basket buttons 
+basketButton.forEach(addEventListenerBasket);
+
+
+
+
 
 
 
